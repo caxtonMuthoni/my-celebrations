bf42e86e83bd97e2179b4ce7841b8b841f8ce2c5
caxtonMuthoni <githinjicaxton323@gmail.com>
2022-05-25 13:27:49 +0300

    Added null checker

--- app/Http/Middleware/EnsureUserIsAsubscriber.php
+++ app/Http/Middleware/EnsureUserIsAsubscriber.php
@@ -20,7 +20,10 @@ class EnsureUserIsAsubscriber
     public function handle(Request $request, Closure $next)
     {
         $subsbscription = Subscriber::with('subscriptionPlan')->where([['user_id', Auth::id()], ['is_active', true]])->first();
-        $books = Book::where('user_id', Auth::id())->whereDate('created_at', '>=', $subsbscription?->created_at)->count();
+        $books = 0;
+        if (isset($subsbscription)) {
+            $books = Book::where('user_id', Auth::id())->whereDate('created_at', '>=', $subsbscription->created_at)->count();
+        }
         if (!isset($subsbscription) && $books < 1) {
             return $next($request);
         }
