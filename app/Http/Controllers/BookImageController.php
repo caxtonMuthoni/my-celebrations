9a7d2321333e95acc247b615ea317b45de0d97f2
Caxton Muthoni <githinjicaxton323@gmail.com>
2022-09-22 15:00:20 +0300

    modified categories page

--- app/Http/Controllers/BookImageController.php
+++ app/Http/Controllers/BookImageController.php
@@ -46,7 +46,6 @@ class BookImageController extends Controller
             $bookId = $request->book_id;
             $userId = Auth::id();
             $book = Book::withCount('bookImages')->with('subscriptionPlan')->findOrFail($request->book_id);
-            dd(count($request->images));
             if(($book->book_images_count + count($request->images)) >= $book->subscriptionPlan->pictures_per_book) {
                 return response()->json([
                     'status' => false,
