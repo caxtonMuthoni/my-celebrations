d20a42f6cb35c81fbef3e509c5ee04076b78ae86
Caxton Muthoni <githinjicaxton323@gmail.com>
2022-12-08 09:17:02 +0300

    modified styles

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Celebration books</title>
-    <link rel="stylesheet" href="{{asset('css/home/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/home/app.css')}}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>My Celebration books</title>
 
 </head>
 
