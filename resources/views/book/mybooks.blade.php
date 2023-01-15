e22ab2cfa61a333185e24964b87b1c51cb6a5cb3
Caxton Muthoni <githinjicaxton323@gmail.com>
2023-01-15 19:04:02 +0300

    added new styles and migrated to new design

--- resources/views/book/mybooks.blade.php
+++ resources/views/book/mybooks.blade.php
@@ -3,75 +3,7 @@
 
 <div class="mybooks container">
     <h3>My books</h3>
-    <div class="row">
-        @foreach($books as $key => $book)
-        <div class="col-md-3">
-            <div class="book-card shadow-sm">
-                <div class="book-card__type">{{$book->category->name}}</div>
-                <div class="book-card__image">
-                    <img class="book-card__image--picture" src="{{$book->image}}" alt="book tittle">
-                </div>
-                <div class="book-card__content">
-                    <div class="book-card__content--header text-secondary">{{ $book->title }}</div>
-                    <div class="book-card__content--description">
-                        {{ $book->cover_message }}
-                    </div>
-                </div>
-                <div class="book-card__cta p-2">
-                    <a href="{{route('book-show', $book->id)}}" title="Edit book content" class="book-card__cta-btn btn btn-sm btn-info">
-                        <i class="fa fa-edit"></i> Content
-                    </a>
-
-                    <a href="{{route('edit-book-details', $book->id)}}" title="Edit cover content" class="book-card__cta-btn btn btn-sm btn-primary">
-                        <i class="fa fa-edit"></i> Cover
-                    </a>
-
-                    <span class="">
-                        @if($book->public)
-                        <span class="btn btn-sm btn-warning">public</span>
-                        @else
-                        <span class="btn btn-sm btn-primary">private</span>
-                        @endif
-                    </span>
-
-                    <span class="">
-                        @if($book->published)
-                        <span class="btn btn-sm btn-success">published</span>
-                        @else
-                        <span class="btn btn-sm btn-info">draft</span>
-                        @endif
-                    </span>
-                </div>
-                <div class="book-card__cta p-2">
-                    <span>
-                        @if($book->accepting_message)
-                        <a href="{{ route('book-view-messages-pictures', $book->id) }}" title="view book messages" class="book-card__cta-btn btn btn-sm btn-primary">
-                            <i class="fa fa-envelope"></i> Messages
-                        </a>
-                        @else
-                        <span class="btn btn-sm btn-primary">No message</span>
-                        @endif
-                    </span>
-
-                    <span>
-                        @if($book->accepting_message)
-                        <a href="{{ route('book-view-messages-pictures', $book->id) }}" title="view book pictures" class="book-card__cta-btn btn btn-sm btn-info">
-                            <i class="fa fa-image"></i> Pictures
-                        </a>
-                        @else
-                        <span class="btn btn-sm btn-primary">No pictures</span>
-                        @endif
-                    </span>
-
-                    <a href="{{route('readBookPDf', $book->id)}}" title="Read book" class="book-card__cta-btn btn btn-sm btn-success">
-                            <i class="fa fa-book"></i> Read
-                        </a>
-                </div>
-            </div>
-        </div>
-        @endforeach
-    </div>
-    <!-- <div class="table-responsive bg-white p-3 hidden" style="display: none;">
+    <div class="table-responsive bg-white p-3">
         <table class="table">
             <thead>
                 <th></th>
@@ -141,7 +73,7 @@
                     <td>
                         <span class="table__item">
                             <a href="{{route('readBookPDf', $book->id)}}" class="btn btn-sm btn-success">
-                                <i class="fa fa-book me-2" aria-hidden="true"></i>
+                            <i class="fa fa-book me-2" aria-hidden="true"></i>
                                 Read
                             </a>
                         </span>
@@ -150,7 +82,7 @@
                 @endforeach
             </tbody>
         </table>
-    </div> -->
+    </div>
 </div>
 
 @endsection
